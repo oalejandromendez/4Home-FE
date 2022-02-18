380e76e5ab89829e14e7084aeda840226a45b885
Camilo Sanchez <camilosp_0525@hotmail.com>
2022-02-18 11:56:22 -0500

    Url servidor para pantalla de confirmacion de pago

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   host: 'https://4homeapi.inkdigital.co',
   payu: 'https://sandbox.checkout.payulatam.com/ppp-web-gateway-payu',
-  responseUrl: 'http://localhost:4200/finance/response',
+  responseUrl: 'https://4home.inkdigital.co/#/finance/response',
   confirmationUrl: 'https://4homeapi.inkdigital.co/api/payment/confirmation',
   apiLogin: 'pRRXKOl8ikMmt9u',
   apiKey: '4Vj8eK4rloUd272L48hsrarnUA',
